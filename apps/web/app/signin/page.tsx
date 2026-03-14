c6f637f4f25ecca844073136f3059ad9bc4775b5
Ram Gavhane <ramgavhane28@gmail.com>
2026-03-14 17:37:49 +0530

    be url config

--- apps/web/app/signin/page.tsx
+++ apps/web/app/signin/page.tsx
@@ -21,7 +21,7 @@ export default function SigninPage() {
       const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/signin"
         , {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: { "Content-Type": "application/json"},
         body: JSON.stringify({ email, password }),
       });
 
